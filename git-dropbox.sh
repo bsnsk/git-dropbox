0d88d1bf2b9e1ea14f098abe2c2747e7d64a46ea
Shuyang Shi <shuyang790@gmail.com>
2016-06-18 19:19:56 +0800

    use CASE to organize argument judgement

--- git-dropbox.sh
+++ git-dropbox.sh
@@ -1,18 +1,19 @@
 #!/bin/bash 
+
 dir=$(pwd)
 
-if [ $# -lt 1 ]
-then
+printHelp()
+{
     echo "git-dropbox Help Information"
     echo -e "FORMAT:"
-    echo -e "\t$ git-dropbox [INSTRUCTION] "
+    echo -e "\t$ git-dropbox [INSTRUCTION]\n"
     echo "INSTRUCTION:"
     echo -e "\tcreate [NAME]\n\t\t-- Create a corresponding repo in ~/Dropbox/git/ directory, NAME is optional"
     echo -e "\tpush \n\t\t-- Push to your dropbox repo (current branch)"
     echo -e "\tpull \n\t\t-- Pull from your dropbox repo (current branch)"
     echo -e "\tlist \n\t\t-- List your repositories in ~/Dropbox/git/"
-    echo -e "\n"
-fi
+    echo ""
+}
 
 if
     git branch 2>&1 | grep "fatal:"
@@ -22,36 +23,38 @@ then # git fatal error
 else # .git is found and fine
 
 branchName=$(git branch | grep \* | awk '{print $2}')
-if [ "$1" = "create" ] 
-then
-    folderName=${dir##*/}
-    if [ $# -gt 1 ] 
-    then
-        folderName=$2
-    fi
-    echo "folder name is ${folderName}"
-    (cd ~/Dropbox; mkdir git; cd git; git init --bare ${folderName}.git)
-    echo "Git Repo ~/Dropbox/git/${folderName}.git created!"
-    git remote remove dropbox
-    git remote add dropbox ~/Dropbox/git/${folderName}.git
-    echo "Git remote added!"
-    git push dropbox ${branchName}
-    echo "Branch '${branchName}' pushed!"
-fi
+case "$1" in 
+    "create")
+        folderName=${dir##*/}
+        if [ $# -gt 1 ] 
+        then
+            folderName=$2
+        fi
+        echo "folder name is ${folderName}"
+        (cd ~/Dropbox; mkdir git; cd git; git init --bare ${folderName}.git)
+        echo "Git Repo ~/Dropbox/git/${folderName}.git created!"
+        git remote remove dropbox
+        git remote add dropbox ~/Dropbox/git/${folderName}.git
+        echo "Git remote added!"
+        git push dropbox ${branchName}
+        echo "Branch '${branchName}' pushed!"
+        ;;
 
-if [ "$1" = "push" ]
-then
-    git push dropbox ${branchName}
-fi
+    "push")
+        git push dropbox ${branchName}
+        ;;
 
-if [ "$1" = "pull" ]
-then
-    git pull dropbox ${branchName}
-fi
+    "pull")
+        git pull dropbox ${branchName}
+        ;;
 
-if [ "$1" = "list" ]
-then
-    ls ~/Dropbox/git
-fi
+    "list")
+        ls ~/Dropbox/git
+        ;;
+
+    *)
+        printHelp 
+        ;;
+esac
 
 fi
